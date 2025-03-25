c15d38100a19934566b57f77402a1cd7f0c445a8
Bao Nguyen <nqb9811@gmail.com>
2025-03-25 16:13:55 +0700

    up

--- test/async-queue.spec.js
+++ test/async-queue.spec.js
@@ -6,13 +6,15 @@ describe('Async queue specs', () => {
     const operationElapsedTimes = [];
     const operationPromises = [];
     const elapsed = performance.now();
+
     let promise = queue.process((resolve) => {
       setTimeout(() => {
         operationElapsedTimes.push(performance.now() - elapsed);
         resolve();
       }, 100);
     });
-    promise = operationPromises.push(promise);
+    operationPromises.push(promise);
+
     queue.process((resolve) => {
       setTimeout(() => {
         operationElapsedTimes.push(performance.now() - elapsed);
@@ -20,6 +22,7 @@ describe('Async queue specs', () => {
       }, 10);
     });
     operationPromises.push(promise);
+
     await Promise.all(operationPromises);
     expect(operationElapsedTimes.every((elapsed) => elapsed >= 100)).toBe(true);
   });
@@ -27,18 +30,21 @@ describe('Async queue specs', () => {
   test('should not break when one operation rejects', async () => {
     const queue = new AsyncQueue();
     const operationPromises = [];
+
     let promise = queue.process((resolve, reject) => {
       setTimeout(() => {
         reject(new Error('Test'));
       }, 50);
     });
     operationPromises.push(promise);
+
     promise = queue.process((resolve) => {
       setTimeout(() => {
         resolve(10);
       }, 10);
     });
     operationPromises.push(promise);
+
     await expect(operationPromises[0]).rejects.toThrow('Test');
     await expect(operationPromises[1]).resolves.toBe(10);
   });
@@ -46,15 +52,17 @@ describe('Async queue specs', () => {
   test('should not break if there is unexpected error thrown in an operation', async () => {
     const queue = new AsyncQueue();
     const operationPromises = [];
+
     let promise = queue.process((resolve, reject) => {
       throw new Error('Test1');
     });
     operationPromises.push(promise);
+
     promise = queue.process((resolve) => {
       return Promise.reject(new Error('Test2'));
     });
     operationPromises.push(promise);
-    await Promise.allSettled(operationPromises);
+
     await expect(operationPromises[0]).rejects.toThrow('Test1');
     await expect(operationPromises[1]).rejects.toThrow('Test2');
   });
--- test/hierarchical-async-queue.spec.js
+++ test/hierarchical-async-queue.spec.js
@@ -6,13 +6,15 @@ describe('Hierarchical async queue specs', () => {
     const operationElapsedTimes = [];
     const operationPromises = [];
     const elapsed = performance.now();
+
     let promise = queue.process('/path/to/test/file', (resolve) => {
       setTimeout(() => {
         operationElapsedTimes.push(performance.now() - elapsed);
         resolve();
       }, 100);
     });
-    promise = operationPromises.push(promise);
+    operationPromises.push(promise);
+
     queue.process('/path/to/test/file', (resolve) => {
       setTimeout(() => {
         operationElapsedTimes.push(performance.now() - elapsed);
@@ -20,7 +22,67 @@ describe('Hierarchical async queue specs', () => {
       }, 10);
     });
     operationPromises.push(promise);
+
     await Promise.all(operationPromises);
     expect(operationElapsedTimes.every((elapsed) => elapsed >= 100)).toBe(true);
+  });
+
+  test('should have additional hierarchical blocking', async () => {
+    const queue = new HierarchicalAsyncQueue('/');
+    const operationElapsedTimes = [];
+    const expectedOperationElapsedTimes = [];
+    const operationPromises = [];
+    const elapsed = performance.now();
+
+    // Sample hierarchy /A/B/C
+    let promise;
+
+    // 1. operation on A, should run immediately
+    promise = queue.process('/a', (resolve) => {
+      setTimeout(() => {
+        operationElapsedTimes.push(performance.now() - elapsed);
+        resolve();
+      }, 100);
+    });
+    operationPromises.push(promise);
+    expectedOperationElapsedTimes.push(100);
+
+    // 2. operation on C, should wait for C operation to complete
+    promise = queue.process('/a', (resolve) => {
+      setTimeout(() => {
+        operationElapsedTimes.push(performance.now() - elapsed);
+        resolve();
+      }, 50);
+    });
+    operationPromises.push(promise);
+    expectedOperationElapsedTimes.push(150);
+
+    // 3. operation on B, should wait for C operation to complete
+    promise = queue.process('/a', (resolve) => {
+      setTimeout(() => {
+        operationElapsedTimes.push(performance.now() - elapsed);
+        resolve();
+      }, 10);
+    });
+    operationPromises.push(promise);
+    expectedOperationElapsedTimes.push(160);
+
+    // 4. second operation on B, should wait for first operation on B to complete
+    promise = queue.process('/a', (resolve) => {
+      setTimeout(() => {
+        operationElapsedTimes.push(performance.now() - elapsed);
+        resolve();
+      }, 30);
+    });
+    operationPromises.push(promise);
+    expectedOperationElapsedTimes.push(190);
+
+    // 5.
+
+    await Promise.all(operationPromises);
+    expect(
+      operationElapsedTimes.every((i, elapsed) => (
+        elapsed >= expectedOperationElapsedTimes[i]),
+      )).toBe(true);
   });
 });
