bef77c5f2e77779e2aff0c3637337defd11bb219
Bao Nguyen <nqb9811@gmail.com>
2025-03-25 16:43:07 +0700

    up

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+type Executor = (resolve: (data: any) => void, reject: (error) => void) => void | Promise<void>;
+
+export class AsyncQueue {
+  process(executor: Executor): Promise<any>;
+}
+
+export class HierarchicalAsyncQueue {
+  constructor(pathSeparator: string);
+  process(path: string, executor: Executor): Promise<any>;
+}
